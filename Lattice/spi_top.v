73502e025f78340ccae0df2e3fa21959538d7715
Ryos Suzuki <ryos@sinby.com>
2017-10-09 00:38:25 +0900

    main worker works well.

--- Lattice/spi_top.v
+++ Lattice/spi_top.v
@@ -1,17 +1,17 @@
 module spi_top (
+    input  wire clk,        // 27M clock
+
     input  wire SPI_MISO, 
     output wire SPI_MOSI, 
     output wire SPI_SCLK, 
     output wire SPI_CS_N, 
 
-    input wire clk, 
-
-    output wire RED_N,
-    output wire BLU_N,
-    output wire GREEN_N);
+    output wire REDn,
+    output wire GREEN_N,
+    output wire BLUE_N);
 
     //----------------------------------------------------------------
-    wire sbus_ack;
+    reg sbus_ack;
     wire sbus_stb; 
     wire sbus_rw; 
     wire [7:0] sbus_addr;
@@ -19,16 +19,45 @@ module spi_top (
     wire [7:0] sbus_data_out;
     wire sbus_led;
 
-    assign sbus_ack = 1'b0;
+    //assign sbus_ack = 1'b0;
     //assign sbus_stb = 1'b0;
     //assign sbus_rw = 1'b0;
 
     //----------------------------------------------------------------
+    wire red_wire;
+    wire green_wire;
+    wire  blue_wire;
+    wire [2:0] debug_wire;
+
+    assign red_wire = debug_wire[2];
+    //assign debug_wire[1] = green_wire;
+    assign blue_wire = debug_wire[0];
+
+    always @(posedge clk) begin
+        if (sbus_stb) begin
+            sbus_ack <= 1'b1;
+        end 
+        if (sbus_ack) begin
+            if (SPI_MISO == 1'b0) begin
+                sbus_ack <= 1'b0;
+            end
+        end 
+    end
+
+    //----------------------------------------------------------------
+    Blink_blink U0 (
+      .clk(clk),
+      .rst(0),
+      .led(green_wire)
+    );
+
+    //----------------------------------------------------------------
     system_bus_sbus sbus(
         .clk(clk),
-        .rst(1'b0),
+        .rst(0),
         .ack(sbus_ack),
         .data_in(sbus_data_in),
+        .debug(debug_wire),
         .stb(sbus_stb),
         .rw(sbus_rw),
         .addr(sbus_addr),
@@ -87,6 +116,24 @@ module spi_top (
 //        .MCSNOE2(SPI2_MCSNoe[2]),
 //        .MCSNOE1(SPI2_MCSNoe[1]),
 //        .MCSNOE0(SPI2_MCSNoe[0]));
+
+    //------------------------------
+    // Instantiate RGB primitives
+    //------------------------------
+    SB_RGBA_DRV RGB_DRIVER (
+      .RGBLEDEN (1'b1),
+      .RGB0PWM  (red_wire),
+      .RGB1PWM  (green_wire),
+      .RGB2PWM  (blue_wire),
+      .CURREN   (1'b1), 
+      .RGB0     (REDn),		//Actual Hardware connection
+      .RGB1     (GREEN_N),
+      .RGB2     (BLUE_N)
+    );
+
+    defparam RGB_DRIVER.RGB0_CURRENT = "0b000001";
+    defparam RGB_DRIVER.RGB1_CURRENT = "0b000001";
+    defparam RGB_DRIVER.RGB2_CURRENT = "0b000001";
 endmodule
 
 
--- Lattice/system_bus.py
+++ Lattice/system_bus.py
@@ -1,8 +1,8 @@
 import polyphony
+from polyphony import testbench, module, is_worker_running
 from polyphony.io import Port
-from polyphony.typing import bit, uint8, uint12, uint24
+from polyphony.typing import bit, uint8, uint3
 from polyphony.timing import clksleep, clkfence, wait_rising, wait_falling, wait_value
-
 
 CONVST_PULSE_CYCLE = 10
 CONVERSION_CYCLE = 39
@@ -18,8 +18,10 @@ class system_bus:
         self.ack = Port(bit, 'in')
 
         self.led = Port(bit, 'out')
+        self.debug = Port(uint3, 'out')
 
-        self.append_worker(self.worker)
+        #self.append_worker(self.worker)
+        self.append_worker(self.main)
 
     def write_data(self, addr:uint8, data:uint8):
         self.rw(1)
@@ -69,29 +71,55 @@ class system_bus:
         data1 = self.read_data(0x0E)
         return (data0 | data1)
 
-    def worker(self):
-        clksleep(10)
-        self.write_data(0x07, 8)
-        self.write_data(0x0F, 1)
-        self.write_data(0x09, 0x80)
-        self.write_data(0x0A, 0x80)
-        self.write_data(0x0B, 11)
+#    def worker(self):
+#        self.debug.wr(7)
+#        clksleep(10)
+#        self.debug.wr(1)
+#
+#        #self.write_data(0x07, 8)
+#        #self.debug.wr(2)
+#
+#        #self.write_data(0x0F, 1)
+#        #self.write_data(0x09, 0x80)
+#        #self.write_data(0x0A, 0x80)
+#        #self.write_data(0x0B, 11)
+#
+#        #clkfence()
+#        #clksleep(10)
+#        #while polyphony.is_worker_running():
+#        #    data16 = self.read_spi_data16()
+#        #    print(data16)
+#
+#        while is_worker_running():
+#            self.led(1)
+#            self._wait()
+#            self.led(0)
+#            self._wait()
 
-        clkfence()
-        clksleep(10)
-        while polyphony.is_worker_running():
-            data16 = self.read_spi_data16()
-            print(data16)
+    def _wait(self):
+        INTERVAL=2000000
+        for i in range(INTERVAL // 2):
+            pass
+
+    def main(self):
+        led:bit = 1
+        debug_v:uint3 = 7
+        while is_worker_running():
+            self.led(led)
+            led = ~led
+            debug_v = 7 ^ debug_v
+            self.debug.wr(debug_v)
+            self._wait()
         
 @polyphony.testbench
 def test(sbus):
-    sbus.ack(0)
-    for i in range(5):
-        wait_rising(sbus.stb)
-        clksleep(1)
-        sbus.ack(1)
-        clkfence()
-        sbus.ack(0)
+#    sbus.ack(0)
+#    for i in range(5):
+#        wait_rising(sbus.stb)
+#        clksleep(1)
+#        sbus.ack(1)
+#        clkfence()
+#        sbus.ack(0)
     
     clksleep(10)
 
