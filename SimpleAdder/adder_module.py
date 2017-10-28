f75f58b85b187087434af64a810e68e1dc251127
Ryos Suzuki <ryos@sinby.com>
2017-10-28 09:02:41 +0900

    adder_module works well.

--- SimpleAdder/adder_module.py
+++ SimpleAdder/adder_module.py
@@ -7,7 +7,7 @@ from polyphony.timing import clksleep, clkfence, wait_rising, wait_falling
 @module
 class adder:
     def __init__(self):
-        self.i_a3  = Port(uint3, 'in', protocol='none')
+        self.i_a3  = Port(uint3, 'in', protocol='valid')
         self.i_b3  = Port(uint3, 'in', protocol='valid')
         self.o_r4  = Port(uint4, 'out', protocol='valid')
         self.append_worker(self.adder_worker)
@@ -15,24 +15,16 @@ class adder:
     def adder_worker(self):
         while polyphony.is_worker_running():
             a3:uint4 = self.i_a3.rd()
-            print("a3:", a3)
             b3:uint4 = self.i_b3.rd()
             r4:uint4 = a3 + b3
             self.o_r4(r4)
 
 m = adder()
 
-def func(m, str, v):
-    print(str)
-    m.i_a3.wr(v)
-
 @testbench
 def test(m):
-    func(m, "hello", 4)
     m.i_a3.wr(3)
-    clkfence()
     m.i_b3.wr(6)
-    clkfence()
     rv = m.o_r4.rd()
     print("rv:", rv)
 
